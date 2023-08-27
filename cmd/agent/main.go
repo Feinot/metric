28032311be2f89e59134e7b5649194876dfd169e
Feinot <gt125.1klass@mail.ru>
2023-08-27 14:34:27 +0700

    'iter2'

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/Feinot/metric/forms"
 	"github.com/Feinot/metric/storage"
+	"log"
 
 	"math/rand"
 	"net/http"
@@ -104,13 +105,19 @@ func GetMet() {
 }
 func MakeGURequest() {
 
-	body, _ := client.Post(fmt.Sprintf("%s%s%s%v", "http://localhost:8080/update/gauge/", storage.M.RandomValue.MName, "/", storage.M.RandomValue.Value), "text/plain", nil)
+	body, err := client.Post(fmt.Sprintf("%s%s%s%v", "http://localhost:8080/update/gauge/", storage.M.RandomValue.MName, "/", storage.M.RandomValue.Value), "text/plain", nil)
 	defer body.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 func MakeCoRequest() {
 
-	body, _ := client.Post(fmt.Sprintf("%s%s%s%v", "http://localhost:8080/update/counter/", storage.M.PollCount.MName, "/", storage.M.PollCount.Value), "text/plain", nil)
+	body, err := client.Post(fmt.Sprintf("%s%s%s%v", "http://localhost:8080/update/counter/", storage.M.PollCount.MName, "/", storage.M.PollCount.Value), "text/plain", nil)
 	defer body.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 func main() {
 
